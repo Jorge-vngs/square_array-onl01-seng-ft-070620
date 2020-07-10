7d261bd940f860ddc21d11dc4327c91b8c6ad350
Jorge Venegas <brangelina01@gmail.com>
2020-07-10 03:30:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+
+ def square_array(array)
+  numbers = [1,2,3]
+  new_array = []  
+  array.each { |numbers| new_array << numbers ** 2 }
+  new_array
 end
